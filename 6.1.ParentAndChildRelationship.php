65884ceb3ea05c4b86f7f0c3ffffd81b1068c65b
naveenchittiphp <74538838+naveenchittiphp@users.noreply.github.com>
2022-05-28 12:26:31 +0530

    Update 6.1.ParentAndChildRelationship.php

--- 6.1.ParentAndChildRelationship.php
+++ 6.1.ParentAndChildRelationship.php
@@ -33,6 +33,22 @@ here the office has many employees so we given employees as a list of employess 
           
           Here user may choose the basket items without login so we use cookies to store the user information 
           and it is a randomly generated id.
+            
+             public void RemoveItem(int ProductId,int Quanity)
+        {
+            var item = Items.FirstOrDefault(item => item.ProductId == ProductId);
+            item.Quanitity -= Quanity;
+            if(item.Quanitity == 0)
+            {
+                Items.Remove(item);
+            }
+        }
+            here we are remove the item from the basket.
+            first we will find the any item matches the productId 
+            if matches then we will reduse the quantity 
+            if quantity is 0 then we will remove the item from basketItem table.
+            
+            
           
           So we initilize the any basket then we will load all items belongs to that basket also.
           
@@ -116,8 +132,204 @@ here the office has many employees so we given employees as a list of employess 
           here we are check any of  the basket items have product.id 
           if we have then we are adding same product to our basket 
           Insted of adding same product to again into the basketItem we will increase the quanity.
-          
-          
+            
+            ------------------------------
+            DbContext
+            -------------------------------
+            In db context we need add Baskets connection only 
+            because we never get any BasketItem induvidually 
+            
+            in Basket we have list of items so here we have one to many relationship.
+            
+            Each BasketItem has one product 
+            
+            since we placed 
+              //Navigation property
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+            while migration we get this 
+             onDelete: ReferentialAction.Cascade
+            meance if we delete a product all basketItem related to the item will deleted.
+            
+            onDelete: ReferentialAction.Restrict
+            here this meance if we delete BasketItem nothing happens to the Products now product will be removed.
+            
+            -------------------
+            Migration 
+            ------------
+            
+            using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreRelationship.Migrations
+{
+    public partial class AddedBasketEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Baskets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BuyerId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Baskets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BasketItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Quanitity = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    BasketId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BasketItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BasketItem_Baskets_BasketId",
+                        column: x => x.BasketId,
+                        principalTable: "Baskets",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_BasketItem_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BasketItem_BasketId",
+                table: "BasketItem",
+                column: "BasketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BasketItem_ProductId",
+                table: "BasketItem",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BasketItem");
+
+            migrationBuilder.DropTable(
+                name: "Baskets");
+        }
+    }
+}
+
+            we get a migration like this 
+            
+            
+          in this   
+            ProductId = table.Column<int>(type: "int", nullable: false),
+            so if we add a record to BasketItem product id should be there it is correct
+                    BasketId = table.Column<int>(type: "int", nullable: true)
+            if we add a item to basket basket id should not be nullable because we need to know which item this basketItems are going.
+            
+            But we dont want this.so we need to add basket (Parent) to basketItem (Child)
+            
+            public int BasketId { get; set; }
+        public Basket Basket { get; set; }
+            
+            Now run migration then the migration will be look like this 
+            
+            using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreRelationship.Migrations
+{
+    public partial class BasketEntityAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Baskets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BuyerId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Baskets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BasketItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Quanitity = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    BasketId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BasketItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BasketItems_Baskets_BasketId",
+                        column: x => x.BasketId,
+                        principalTable: "Baskets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BasketItems_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BasketItems_BasketId",
+                table: "BasketItems",
+                column: "BasketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BasketItems_ProductId",
+                table: "BasketItems",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BasketItems");
+
+            migrationBuilder.DropTable(
+                name: "Baskets");
+        }
+    }
+}
+
+            
+            Now we can see some changes 
+            
+             ProductId = table.Column<int>(type: "int", nullable: false),
+                    BasketId = table.Column<int>(type: "int", nullable: false)
+            
+            so if we create an BasketItem product id and Basket Id should be there.
+            
+            if we remove the bakset item or product 
+            
+            basket item should be alse deleted.
+            
+            
          
           
           
